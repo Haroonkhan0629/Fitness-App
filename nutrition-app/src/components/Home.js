6be6ef192c50260b99c1fd91bb026c251a6b1735
Haroon Khan <haroonkhan0629@users.noreply.github.com>
2026-04-05 22:14:34 -0400

    fix

--- nutrition-app/src/components/Home.js
+++ nutrition-app/src/components/Home.js
@@ -46,7 +46,6 @@ class Home extends Component {
           <div>
             <nav aria-label="breadcrumb">
               <ol class="breadcrumb">
-                <li class="breadcrumb-item"><Link to="/">Home</Link></li>
                 <li class="breadcrumb-item active" aria-current="page">Exercises</li>
               </ol>
             </nav>
